8141380da10fbd089c5dcc64b7d9efff94f71205
Damon Jellema <damon.jellema2004@gmail.com>
2020-10-22 09:48:00 +0200

    Zon

---
+++ 01-Turtle/damon.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.pencolor("Yellow")
+turtle.fillcolor("Orange")
+turtle.begin_fill()
+turtle.speed(0)
+
+
+
+for x in range(36):
+    
+        turtle.forward(100)
+        turtle.left(45)
+        turtle.forward(100)
+        turtle.left(45)
+        turtle.forward(100)
+        turtle.left(10)
+        
+turtle.end_fill()
+turtle.done()    
